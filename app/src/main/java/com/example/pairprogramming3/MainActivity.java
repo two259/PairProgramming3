0299deeb37faf34a2febff07d4560a9cf227c3ba
two259 <two259@vt.edu>
2021-04-15 11:43:07 -0400

    First changes

--- app/src/main/java/com/example/pairprogramming3/MainActivity.java
+++ app/src/main/java/com/example/pairprogramming3/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.pairprogramming3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button pictureButton;
+    ImageView picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pictureButton = findViewById(R.id.pictureButton);
+        picture = findViewById(R.id.imageView);
     }
 }
---
+++ app/src/main/java/com/example/pairprogramming3/TouchListener.java
@@ -0,0 +1,65 @@
+package com.example.pairprogramming3;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.pairprogramming3.MainActivity;
+
+
+public class TouchListener implements View.OnTouchListener {
+    MainActivity mainActivity;
+    GestureDetectorCompat gestureDetectorCompat;
+
+    public TouchListener(MainActivity ma) {
+        this.mainActivity = ma;
+        gestureDetectorCompat = new GestureDetectorCompat(this.mainActivity, new MyGestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        gestureDetectorCompat.onTouchEvent(motionEvent);
+        int maskedAction = motionEvent.getActionMasked();
+        switch(maskedAction){
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
+                for(int i= 0, size = motionEvent.getPointerCount(); i< size; i++){
+                    int id = motionEvent.getPointerId(i);
+                    mainActivity.addPath(id, motionEvent.getX(i), motionEvent.getY(i));
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                for(int i= 0, size = motionEvent.getPointerCount(); i< size; i++){
+                    int id = motionEvent.getPointerId(i);
+                    mainActivity.updatePath(id, motionEvent.getX(i), motionEvent.getY(i));
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL:
+                for(int i= 0, size = motionEvent.getPointerCount(); i< size; i++){
+                    int id = motionEvent.getPointerId(i);
+                    mainActivity.removePath(id);
+                }
+                break;
+        }
+        return true;
+    }
+
+
+
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            mainActivity.onDoubleTap();
+            return super.onDoubleTap(e);
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            mainActivity.onLongPress();
+            super.onLongPress(e);
+        }
+    }
+}
